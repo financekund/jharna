1a13823de5a058e291bca5634d16f0598ecf1d12
financekund <financeplanhelp@gmail.com>
2025-09-03 11:42:31 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // root par deploy
-});
+  base: '/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8788', // Wrangler dev server for Cloudflare Functions
+    },
+  },
+})
